5f6d02c23b5173435b3dcd3ecae853bcb3ea370f
Eric Haase <erichaase@gmail.com>
2014-08-30 04:46:46 -0500

    generalized spec for json data

--- ruby/spec/AdvertisingAgency_spec.rb
+++ ruby/spec/AdvertisingAgency_spec.rb
@@ -1,4 +1,7 @@
-require_relative '../app/AdvertisingAgency'
+require_relative "../app/%s" % [File.basename(__FILE__, "_spec.rb")]
+
+path = __FILE__.sub(/\.rb$/, ".json")
+$DATA = JSON.parse(File.read(path))
 
 describe AdvertisingAgency do
   after(:each) do
@@ -6,28 +9,10 @@ describe AdvertisingAgency do
     expect(results).to eq(@expected)
   end
 
-  it "test 1" do
-    @requests = [1, 2, 3]
-    @expected = 0
-  end
-
-  it "test 2" do
-    @requests = [1, 1, 1]
-    @expected = 2
-  end
-
-  it "test 3" do
-    @requests = [1, 2, 1, 2]
-    @expected = 2
-  end
-
-  it "test 4" do
-    @requests = [24, 51, 23, 96, 51, 73, 30, 42, 44, 20, 20, 20, 58, 23, 44, 35, 2, 42, 20, 74, 24, 37, 68, 95, 34, 37, 42, 37, 95, 68, 57, 20, 95, 24, 24, 20, 20, 10, 44, 30, 47, 42, 72, 57, 10, 47, 30, 68, 96]
-    @expected = 28
-  end
-
-  it "test 5" do
-    @requests = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-    @expected = 49
+  $DATA.each_with_index do |t, i|
+    it "test #{i}" do
+      @requests = t[0]
+      @expected = t[1]
+    end
   end
 end

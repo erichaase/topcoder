5a8ca8187f547ff206196cd2fddf164e7a371ef5
Eric Haase <erichaase@gmail.com>
2014-10-19 19:11:36 -0500

    added Java infrastructure and HuffmanDecoding:java problem

---
+++ java/src/main/java/com/github/erichaase/HuffmanDecoding.java
@@ -0,0 +1,44 @@
+/**
+ * @see <a href="http://community.topcoder.com/stat?c=problem_statement&pm=6477">HuffmanDecoding</a>
+ */
+
+package com.github.erichaase;
+
+import java.util.*;
+import org.json.simple.*;
+
+public class HuffmanDecoding {
+    public static Object decode (JSONArray args) {
+        // input / output
+        String    encodedIn = (String)    args.get(0);
+        JSONArray dictIn    = (JSONArray) args.get(1);
+        String    decoded   = "";
+
+        // intermediate data structures
+        List<String>            encoded = new ArrayList<String>();
+        HashMap<String, String> dict    = new HashMap<String, String>();
+
+        // build List of encoded string chars
+        for (int i = 0; i < encodedIn.length(); i++) {
+            String c = Character.toString(encodedIn.charAt(i));
+            encoded.add(c);
+        }
+
+        // build HashMap dictionary
+        char c = 'A'; int i = 0;
+        for (Object key : dictIn) {
+            dict.put((String) key, Character.toString(c));
+            ++c; ++i;
+        }
+
+        // build decoded string
+        while (encoded.size() != 0) {
+            String cur = encoded.remove(0);
+            while (! dict.containsKey(cur))
+                cur += encoded.remove(0);
+            decoded += dict.get(cur);
+        }
+
+        return decoded;
+    }
+}
---
+++ java/src/test/java/com/github/erichaase/HuffmanDecodingTest.java
@@ -0,0 +1,26 @@
+package com.github.erichaase;
+
+import java.io.*;
+import org.apache.commons.io.FileUtils;
+
+import        org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.json.simple.*;
+import org.json.simple.parser.ParseException;
+
+public class HuffmanDecodingTest {
+    @Test
+    public void test() throws IOException, ParseException {
+        File   f = new File(this.getClass().getResource("/HuffmanDecodingTest.json").getFile());
+        String s = FileUtils.readFileToString(f, "UTF-8");
+
+        for (Object testcase : (JSONArray) JSONValue.parse(s)) {
+            JSONArray args     = (JSONArray) testcase;
+            Object    expected = args.remove(args.size() - 1);
+
+            Object result = HuffmanDecoding.decode(args);
+            assertEquals(expected, result);
+        }
+    }
+}

d09d8539c5f6fbb86e22686c1c30fb5a25a35da0
Eric Haase <erichaase@gmail.com>
2014-10-21 19:51:26 -0500

    added ComplementaryDNAChains.java problem

---
+++ java/src/main/java/com/github/erichaase/ComplementaryDNAChains.java
@@ -0,0 +1,31 @@
+/**
+ * @see <a href="http://community.topcoder.com/stat?c=problem_statement&pm=8114">ComplementaryDNAChains</a>
+ */
+
+package com.github.erichaase;
+
+import java.util.*;
+import org.json.simple.*;
+
+public class ComplementaryDNAChains {
+    public static Object minReplaces (JSONArray args) {
+        // input / output
+        String first  = (String) args.get(0);
+        String second = (String) args.get(1);
+        long   n      = 0;
+
+        for (int i = 0; i < first.length(); i++) {
+            char a = first.charAt(i);
+            char b = second.charAt(i);
+
+            if      (((a == 'A') && (b == 'T')) || ((a == 'T') && (b == 'A')))
+                continue;
+            else if (((a == 'C') && (b == 'G')) || ((a == 'G') && (b == 'C')))
+                continue;
+            else
+                ++n;
+        }
+
+        return n;
+    }
+}

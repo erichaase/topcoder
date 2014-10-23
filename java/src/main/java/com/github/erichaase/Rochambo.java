a3ab31d2f183d69b743ab38f9ec69b565b1d4116
Eric Haase <erichaase@gmail.com>
2014-10-23 10:30:38 -0500

    added Rochambo.java problem

---
+++ java/src/main/java/com/github/erichaase/Rochambo.java
@@ -0,0 +1,56 @@
+/**
+ * @see <a href="http://community.topcoder.com/stat?c=problem_statement&pm=1810">Rochambo</a>
+ */
+
+package com.github.erichaase;
+
+import java.util.*;
+import org.json.simple.*;
+
+public class Rochambo {
+    private static final Map<Character, Character> beats;
+    static {
+        beats = new HashMap<Character, Character>();
+        beats.put('R', 'P');
+        beats.put('P', 'S');
+        beats.put('S', 'R');
+    }
+
+    private static final Map<String, Character> next;
+    static {
+        next = new HashMap<String, Character>();
+        next.put("RR", 'R');
+        next.put("PP", 'P');
+        next.put("SS", 'S');
+        next.put("PS", 'R');
+        next.put("SP", 'R');
+        next.put("RS", 'P');
+        next.put("SR", 'P');
+        next.put("RP", 'S');
+        next.put("PR", 'S');
+    }
+
+    public static Object solution (JSONArray args) {
+        // input / output
+        String opponent = (String) args.get(0);
+        long   n        = 0;
+
+        // calculate n
+        for (int i = 0; i < opponent.length(); i++) {
+            char mine, theirs = opponent.charAt(i);
+
+            if (i < 2)
+                mine = 'R';
+            else {
+                String first  = Character.toString(opponent.charAt(i-2));
+                String second = Character.toString(opponent.charAt(i-1));
+                       mine   = beats.get(next.get(first + second));
+            }
+
+            if (beats.get(theirs) == mine)
+                ++n;
+        }
+
+        return n;
+    }
+}
---
+++ java/src/test/java/com/github/erichaase/RochamboTest.java
@@ -0,0 +1,28 @@
+package com.github.erichaase;
+
+import java.io.*;
+import org.apache.commons.io.FileUtils;
+
+import        org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.json.simple.*;
+import org.json.simple.parser.ParseException;
+
+public class RochamboTest {
+    private static final String TEST_DATA_PATH = "/RochamboTest.json";
+
+    @Test
+    public void test() throws IOException, ParseException {
+        File   f = new File(this.getClass().getResource(TEST_DATA_PATH).getFile());
+        String s = FileUtils.readFileToString(f, "UTF-8");
+
+        for (Object testcase : (JSONArray) JSONValue.parse(s)) {
+            JSONArray args     = (JSONArray) testcase;
+            Object    expected = args.remove(args.size() - 1);
+
+            Object result = Rochambo.solution(args);
+            assertEquals(expected, result);
+        }
+    }
+}
